26e3bc9583c5e0cb2a905a6f531c7c61706a40df
IVRING18 <1141502381@qq.com>
2023-10-20 14:11:52 +0800

    MOD:[贪心算法]

--- src/com/leetcode/api/leetcode/baseThoughtImprove/BaseArrayQueueStack.java
+++ src/com/leetcode/api/leetcode/baseThoughtImprove/BaseArrayQueueStack.java
@@ -1011,4 +1011,88 @@ public class BaseArrayQueueStack {
         }
         return res;
     }
+
+    /**
+     * 55. 跳跃游戏
+     * 已解答
+     * 中等
+     * 相关标签
+     * 相关企业
+     * 给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     *
+     * 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,3,1,1,4]
+     * 输出：true
+     * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+     * 示例 2：
+     *
+     * 输入：nums = [3,2,1,0,4]
+     * 输出：false
+     * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+     */
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        int rightMost = 0;
+        for(int i = 0; i < len ; i++) {
+            if(i <= rightMost) {
+                rightMost = Math.max(rightMost, i + nums[i]);
+                if(rightMost >= len - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 45. 跳跃游戏 II
+     * 已解答
+     * 中等
+     * 相关标签
+     * 相关企业
+     * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+     *
+     * 每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+     *
+     * 0 <= j <= nums[i]
+     * i + j < n
+     * 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+     *
+     *
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [2,3,1,1,4]
+     * 输出: 2
+     * 解释: 跳到最后一个位置的最小跳跃数是 2。
+     *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+     * 示例 2:
+     *
+     * 输入: nums = [2,3,0,1,4]
+     * 输出: 2
+     *
+     * https://leetcode.cn/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150
+     */
+    public int jump(int[] nums) {
+        //1、记录当前0角标时，最远maxPosition；并记录一步step
+        //2、继续遍历 至 maxPostion，并计算最远position。 设置最远maxPosition为下一个循环结束标记；
+        //3、直到记录i == end; 或者循环结束
+
+        int len = nums.length;
+        int maxPosition = 0;
+        int end = 0;
+        int step = 0;
+        for(int i = 0; i < len - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if(i == end) {
+                end = maxPosition;
+                step ++;
+            }
+        }
+        return step;
+    }
+
 }
